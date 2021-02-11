18cd7fc8b39b4c441eba5d38142d11ffbfb0b7fc
Chad Altenberger <chadaltenberger@Chads-MBP.attlocal.net>
2021-02-11 09:47:09 -0500

    Initial setup

---
+++ public/browser.js
@@ -0,0 +1,23 @@
+document.addEventListener('click', e => {
+	if (e.target.classList.contains('edit-me')) {
+		let userInput = prompt(
+			'Edit here',
+			e.target.parentElement.parentElement.querySelector('.item-text').innerHTML
+		);
+		if (userInput) {
+			axios
+				.post('/update-item', {
+					text: userInput,
+					id: e.target.getAttribute('data-id')
+				})
+				.then(() => {
+					e.target.parentElement.parentElement.querySelector(
+						'.item-text'
+					).innerHTML = userInput;
+				})
+				.catch(() => {
+					console.log('Please try again later');
+				});
+		}
+	}
+});
